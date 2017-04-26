7d333172fc48b3d09857fb3734eed3ac40a93cc9
Kasra Zahir <kasrazahir@gmail.com>
2017-04-26 11:34:32 -0400

    lint clean up

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Rx from 'rxjs/Rx';
 
 import './App.css';
 import * as payloaders from './payloaders';
-import { apiCall } from './utils';
+import { apiCall } from './utils/apis';
+import { firebaseKey , firebaseAPI } from './private/keys';
 
 const yahooUrl = ticker => 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.dividendhistory%20where%20symbol%20%3D%20%22' + ticker + '%22%20and%20startDate%20%3D%20%221900-01-01%22%20and%20endDate%20%3D%20%222013-12-31%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
 const morningStarUrl = ticker => 'http://financials.morningstar.com/ajax/exportKR2CSV.html?region=USA&t=' + ticker;
@@ -36,6 +37,7 @@ const clicked = () => {
 
 const canceled = () => {
 	console.log('holla cancel');
+	console.log(fireBaseAPI)
 };
 
 class App extends Component {
